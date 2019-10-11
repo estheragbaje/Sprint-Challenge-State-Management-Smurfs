85799bd634714352d298c2f51ead17eb2c4f64fb
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-11 09:04:21 +0100

    Created the action creators

---
+++ smurfs/src/state/actionCreators.js
@@ -0,0 +1,21 @@
+import * as types from "./actionTypes";
+
+export const getData = () => dispatch => {
+  dispatch({ type: types.IS_LOADING });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: types.GET_DATA, payload: res.data });
+    })
+    .catch(err => {
+      dispatch(error(err));
+    });
+};
+
+export function error(err) {
+  return { type: types.ERROR, payload: err };
+}
+
+export function isLoading() {
+  return { type: types.IS_LOADING };
+}
