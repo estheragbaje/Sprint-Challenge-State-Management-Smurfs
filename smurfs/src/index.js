460e849b335548c9ee05e87b479f6f7f893d5e48
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-11 09:15:47 +0100

    Combined reducer and created store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import * as reducers from "./state/reducers";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const monsterReducer = combineReducers({
+  state: reducers.smurfsReducer
+});
+
+//the store that stores everything
+const store = createStore(
+  monsterReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
